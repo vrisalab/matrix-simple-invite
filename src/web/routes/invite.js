915296e66657da9e7806b3aec955003d0bc964d9
vrisalab <v.risalab@gmail.com>
2026-02-23 19:35:53 -0300

    Added redirect back to link if user needed to login

--- src/web/routes/invite.js
+++ src/web/routes/invite.js
@@ -51,13 +51,9 @@ as.router.get("/gg", defineEventHandler(async event => {
 	const {id: invite_link_id} = await getValidatedQuery(event, schema.gg.parse)
 	const session = await auth.useSession(event)
 
-	const link = from("invite_link").select("room_name", "room_icon", "creator_name", "creator_icon", "room_id", "uses", "max_uses", "expiration_date").where({id: invite_link_id}).get()
+	const link = from("invite_link").select("id","room_name", "room_icon", "creator_name", "creator_icon", "room_id", "uses", "max_uses", "expiration_date").where({id: invite_link_id}).get()
 
 	if (link){
-		// Check if you are logged in
-		if (!session.data.mxid){
-			return pugSync.render(event, "not_logged_in.pug", {})
-		}
 		const room_name = link.room_name
 		const room_icon = link.room_icon
 		const creator_name = link.creator_name
@@ -67,7 +63,7 @@ as.router.get("/gg", defineEventHandler(async event => {
 		let validityError = isValidInvite(link)
 		if (validityError) return pugSync.render(event, "invalid_link.pug", {validityError})
 
-		return pugSync.render(event, "gg.pug", {session,  room_name, room_icon, creator_name, creator_icon, invite_link_id})
+		return pugSync.render(event, "gg.pug", {session,  room_name, room_icon, creator_name, creator_icon, invite_link_id, next: "/gg?id=" + link.id})
 	} else {
 		return pugSync.render(event, "invalid_link.pug", {validityError: "Link not found"})
 	}
